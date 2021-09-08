e14ee62fa1e8abd902e106268dbec622f689b001
eliaraguso <elia.raguso@gmail.com>
2021-09-08 12:34:13 +0200

    aggiunta funzione con ciclo for nello snack 3

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,23 +1,37 @@
 // Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 
 
-function reverseString (string){
+// function reverseString (string){
     
-    // uso il metodo split per creare un array composto dalle lettere della mia stringa
-     var splitString = string.split("")
+//     // uso il metodo split per creare un array composto dalle lettere della mia stringa
+//      var splitString = string.split("")
      
   
-    //  uso il metodo reverse per invertire gli elementi dell'array "splitString"
-     var reverseArray = splitString.reverse();
+//     //  uso il metodo reverse per invertire gli elementi dell'array "splitString"
+//      var reverseArray = splitString.reverse();
      
 
-    //  uso il metodo join per unire gli elementi dell'array "reverseArray" trasformando l'array in una stringa
-     var joinArray = reverseArray.join(""); 
+//     //  uso il metodo join per unire gli elementi dell'array "reverseArray" trasformando l'array in una stringa
+//      var joinArray = reverseArray.join(""); 
     
      
-    //  faccio ritornare la stringa iniziale girata
-     return joinArray; 
+//     //  faccio ritornare la stringa iniziale girata
+//      return joinArray; 
+// }
+
+function reverseString(string) {
+    
+    var newString = "";
+ 
+
+    for (var i = string.length - 1; i >= 0; i--) { 
+        newString += string[i];
+    }
+    
+    return newString; 
 }
+ 
+reverseString('hello');
 
 let parolaUtente = (prompt("Inserisci una parola"))
 
