d0a9ae7d390dbfeab605765ef7058be36b9417fd
eliaraguso <elia.raguso@gmail.com>
2021-09-08 13:40:29 +0200

    aggiunto controllo sulla lunghezza degli array iniziali in snack 4

--- snack-4/main.js
+++ snack-4/main.js
@@ -3,18 +3,29 @@
 //     es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
 let array1 = ["a","b","c"];
-let array2 = [1,2,3];
+let array2 = [1,2,3,4];
 let finalArray = [];
 
 
 
 function myFunction (arr1, arr2) {
 
-    for(i=0; i < array1.length; i++) {
-        finalArray.push(array1[i]);
-        finalArray.push(array2[i]);
-    }
+    if (array1.length <= array2.length) {
+        for(i=0; i < array1.length; i++) {
+            finalArray.push(array1[i]);
+            finalArray.push(array2[i]);
+        }
+    } else if(array1.length >= array2.length){
+        for(i=0; i < array2.length; i++) {
+            finalArray.push(array1[i]);
+            finalArray.push(array2[i]);
+        }
 
+    }
+    
+    
+    
+    
     return finalArray;
 }
 
