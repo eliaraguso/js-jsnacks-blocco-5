a2c8fc9f3dcb8a676fe746c31dabe3cb2f932cce
eliaraguso <elia.raguso@gmail.com>
2021-09-08 12:23:45 +0200

    fine snack 3

---
+++ snack-3/main.js
@@ -0,0 +1,31 @@
+// Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+
+
+function reverseString (string){
+    
+    // uso il metodo split per creare un array composto dalle lettere della mia stringa
+     var splitString = string.split("")
+     
+  
+    //  uso il metodo reverse per invertire gli elementi dell'array "splitString"
+     var reverseArray = splitString.reverse();
+     
+
+    //  uso il metodo join per unire gli elementi dell'array "reverseArray" trasformando l'array in una stringa
+     var joinArray = reverseArray.join(""); 
+    
+     
+    //  faccio ritornare la stringa iniziale girata
+     return joinArray; 
+}
+
+let parolaUtente = (prompt("Inserisci una parola"))
+
+let parolaGirata = reverseString(parolaUtente);
+
+
+
+alert("Hai inserito la parola" + " " + parolaUtente);
+
+alert("Guarda come ti giro la parola:" + " " + parolaGirata);
+
